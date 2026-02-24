b03a70ceaa72de2b37be0b9a20d5985fa1e5d598
NiharikaQA <niharikareddyb27@gmail.com>
2026-02-25 00:49:41 +0530

    message updated

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,5 +1,5 @@
 public class Test1 {
     public static void main (String args[]){
-        System.out.println("Hello Test1 java updated from dev ##");
+        System.out.println("Hello master");
     }
 }
